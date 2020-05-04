6f03562b6bbe8502e24bbf19c6cf5d5b49f67927
LiuYongfeng <531817981@qq.com>
2020-05-04 11:12:38 +0800

    first commit

---
+++ Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csharp_test
+{
+    class Person
+    {
+        public Person()
+        {
+            Knowledge = new List<string>();
+        }
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public Person(string name, int id) : this(name)
+        {
+            Id = id;
+        }
+        public Person(string name, int id, int age) : this(name, id)
+        {
+            Age = age;
+        }
+        public string Name;
+        public int Id;
+        public int Age;
+        public List<string> Knowledge;
+        public void Print()
+        {
+            System.Console.WriteLine(Name);
+            System.Console.WriteLine(Id);
+            System.Console.WriteLine(Age);
+        }
+    }
+    struct StructPerson
+    {
+        public string Name;
+        public int Id;
+        public int Age;
+        public void Print()
+        {
+            System.Console.WriteLine(Name);
+            System.Console.WriteLine(Id);
+            System.Console.WriteLine(Age);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace csharp_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person1 = new Person();
+            var person2 = new StructPerson();
+
+            System.Console.WriteLine(person1.ToString());
+            person1.Print();
+            var person3 = person1;
+            person1.Age = 26;
+            person3.Print();
+            System.Console.WriteLine(person2.ToString());
+            person2.Print();
+            var person4 = person2;
+            person2.Age = 26;
+            person4.Print();
+        }
+    }
+}
